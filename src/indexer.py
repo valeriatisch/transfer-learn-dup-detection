cb1c6d3e7b3442c12ff0afbd5698d25834f21d83
valeriatisch <valeria.tisch97@gmail.com>
2023-12-04 14:55:59 +0100

    added multi key indexing

--- src/indexer.py
+++ src/indexer.py
@@ -1,6 +1,6 @@
 import logging
 from configparser import ConfigParser
-from typing import Dict
+from typing import Dict, List
 
 import pandas as pd
 import recordlinkage as rl
@@ -25,6 +25,7 @@ class Indexer:
         candidate_set = ds_dict.get("candidate_set")
         if candidate_set is not None:
             try:
+                # TODO: auslagern in eigene Funktion
                 ltable, rtable = ds_dict.get("tables")
                 # Store original indices
                 ltable_original_index = ltable.reset_index().index.copy()
@@ -60,101 +61,69 @@ class Indexer:
                 if ds_dict.get("pair_method")
                 else self.configparser.default_pair_method
             )
-            tables = ds_dict.get("tables")
-            multi_index = (
-                self.index_single_table(tables[0], method, ds_id)
-                if len(tables) == 1
-                else self.index_two_tables(tables[0], tables[1], method, ds_id)
+            number_indexing_keys = (
+                ds_dict.get("number_indexing_keys", None)
+                if ds_dict.get("number_indexing_keys")
+                else self.configparser.default_number_indexing_keys
             )
+            tables = ds_dict.get("tables")
+            df1 = tables[0]
+            df2 = tables[1] if len(tables) == 2 else df1
+            keys = self.get_highest_entropy_common_columns(
+                df1, df2.columns, number_indexing_keys
+            )
+            multi_index = self.index(df1, df2, keys, method, ds_id)
         return multi_index
-
-    def index_single_table(
-        self, df: pd.DataFrame, method: str, ds_id: str
-    ) -> pd.MultiIndex:
-        key = max(self.calculate_entropy(df), key=self.calculate_entropy(df).get)
-        return self.index(df, df, key, method, ds_id)
-
-    def index_two_tables(
-        self, df1: pd.DataFrame, df2: pd.DataFrame, method: str, ds_id: str
-    ) -> pd.MultiIndex:
-        key = self.find_highest_entropy_common_column(df1, df2)
-        return self.index(df1, df2, key, method, ds_id)
 
     # TODO: maybe for later: in case of two tables assume that they might be dirty by itself,
     #  then indexing and comparing should be performed not only between the two tables but also within each table
     #  Note: the two tables might have different schemas
 
+    @staticmethod
     def index(
-        self, df1: pd.DataFrame, df2: pd.DataFrame, key: str, method: str, ds_id: str
+        df1: pd.DataFrame, df2: pd.DataFrame, keys: List[str], method: str, ds_id: str
     ) -> pd.MultiIndex:
         logging.info(
-            f"Indexing tables of {ds_id} dataset with method {method} and key {key}"
+            f"Indexing tables of {ds_id} dataset with method {method} and keys {keys}"
         )
-        indexer = rl.Index()
-        indexing_methods = {
-            "block": Block(on=key),
-            "sortedneighbourhood": SortedNeighbourhood(on=key),
-            "full": Full(),
-            "random": Random(42),
-        }
-        # TODO: implement multi key indexing properly (for-loop & unify), test and refactor
-        multi_key_indexing = False
-        if multi_key_indexing:
-            combined_index = pd.MultiIndex(levels=[[], []], codes=[[], []])
-            entropies = self.calculate_entropy(df1)
-            keys = 3
-            for key_col in sorted(entropies, key=entropies.get, reverse=True):
-                if keys == 0:
-                    break
-                # We need to find a common column for indexing in case the two tables have different schemas
-                if key_col in df2.columns:
-                    indexing_methods = {
-                        "block": Block(on=key_col),
-                        "sortedneighbourhood": SortedNeighbourhood(on=key_col),
-                        "full": Full(),
-                        "random": Random(42),
-                    }
-                    indexer.add(
-                        indexing_methods.get(
-                            method, ValueError(f"Invalid pair method: {method}")
-                        )
-                    )
-                    pairs = indexer.index(df1, df2)
-                    combined_index = combined_index.union(pairs)
-                    keys -= 1
-            return indexer.index(df1, df2)
-        else:
+        combined_index = pd.MultiIndex(levels=[[], []], codes=[[], []])
+        for key_col in keys:
+            indexer = rl.Index()
+            # We need to find a common column for indexing in case the two tables have different schemas
+            indexing_methods = {
+                "block": Block(on=key_col),
+                "sortedneighbourhood": SortedNeighbourhood(on=key_col),
+                "full": Full(),
+                "random": Random(42),
+            }
             indexer.add(
                 indexing_methods.get(
                     method, ValueError(f"Invalid pair method: {method}")
                 )
             )
-            return indexer.index(df1, df2)
+            pairs = indexer.index(df1, df2)
+            combined_index = combined_index.union(pairs)
+        return combined_index
 
-    def find_lowest_entropy_common_column(
-        self, df1: pd.DataFrame, df2: pd.DataFrame
-    ) -> str:
+    def get_highest_entropy_common_columns(
+        self, df1: pd.DataFrame, df2_columns: List[str], number_indexing_keys: int
+    ) -> List[str]:
         entropies_df1 = self.calculate_entropy(df1)
-        for col in sorted(entropies_df1, key=entropies_df1.get):
-            if col in df2.columns:
-                return col
-        raise ValueError("No common column found for indexing")
-
-    def find_highest_entropy_common_column(
-        self, df1: pd.DataFrame, df2: pd.DataFrame
-    ) -> str:
-        entropies_df1 = self.calculate_entropy(df1)
+        common_columns = []
         for col in sorted(entropies_df1, key=entropies_df1.get, reverse=True):
+            if number_indexing_keys == 0:
+                return common_columns
             # We need to find a common column for indexing in case the two tables have different schemas
-            if col in df2.columns:
-                return col
+            if col in df2_columns:
+                common_columns.append(col)
+            number_indexing_keys -= 1
         raise ValueError("No common entropy column found for indexing")
 
     @staticmethod
     def calculate_entropy(df: pd.DataFrame) -> Dict[str, float]:
         entropies = {}
         for col in df.columns:
-            if preprocessor.is_column_id(df[col]):
+            if preprocessor.is_column_id(df[col]) or col == "id":
                 continue
             counts = df[col].value_counts(normalize=True)
             entropies[col] = entropy(counts)
